4999b60888b10686781dbe64af3ff4ce78003502
Inkdg8 <inkdg8@gmail.com>
2025-12-10 18:41:59 -0800

    Update app.js

--- assignments/5/app.js
+++ assignments/5/app.js
@@ -26,7 +26,7 @@ app.get("/result", (req, res) => {
     });
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+
